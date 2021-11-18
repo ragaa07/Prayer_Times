92fac9ffae286a74e37544dc9c0f7de44b89cc72
ragaa aboelezz <mragaa07@gmail.com>
2021-11-18 09:23:39 +0200

    refactor the permission handling

--- app/src/main/java/com/example/prayertimes/presentation/prayer_times/MainActivity.kt
+++ app/src/main/java/com/example/prayertimes/presentation/prayer_times/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -22,13 +23,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.example.prayertimes.R
 import com.example.prayertimes.presentation.ui.theme.Blue900
 import com.example.prayertimes.presentation.ui.theme.PrayerTimesTheme
 import com.example.prayertimes.presentation.util.Constant.REQUEST_CODE_LOCATION_PERMISSION
 import com.example.prayertimes.presentation.util.changeStatusBarColor
 import com.example.prayertimes.presentation.util.formatDate
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import dagger.hilt.android.AndroidEntryPoint
@@ -43,19 +49,43 @@ class MainActivity : ComponentActivity() {
     private var long: String? by mutableStateOf(null)
     private var gpsIsOf by mutableStateOf(false)
 
+    @ExperimentalPermissionsApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         changeStatusBarColor(Blue900.toArgb())
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        getLocation()
         val startDate = Calendar.getInstance()
         startDate.add(Calendar.MONTH, -1)
         val endDate = Calendar.getInstance()
         endDate.add(Calendar.MONTH, 1)
 
         setContent {
+            val permission = rememberPermissionState(
+                permission =
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+
+            val lifecycleOwner = LocalLifecycleOwner.current
+            DisposableEffect(
+                key1 = lifecycleOwner,
+                effect = {
+                    val observer = LifecycleEventObserver { _, event ->
+                        if (event == Lifecycle.Event.ON_START && !permission.hasPermission) {
+                            permission.launchPermissionRequest()
+                        }
+                    }
+                    lifecycleOwner.lifecycle.addObserver(observer)
+
+                    onDispose {
+                        lifecycleOwner.lifecycle.removeObserver(observer)
+                    }
+                }
+            )
+            if (permission.hasPermission) {
+                getLocation()
+            }
+
             PrayerTimesTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     if (gpsIsOf) {
                         Box(
@@ -92,20 +122,12 @@ class MainActivity : ComponentActivity() {
             if (ContextCompat.checkSelfPermission(
                     this,
                     android.Manifest.permission.ACCESS_FINE_LOCATION,
-                ) == PackageManager.PERMISSION_DENIED &&
+                ) == PackageManager.PERMISSION_GRANTED &&
                 ContextCompat.checkSelfPermission(
                     this,
                     android.Manifest.permission.ACCESS_FINE_LOCATION,
-                ) == PackageManager.PERMISSION_DENIED
+                ) == PackageManager.PERMISSION_GRANTED
             ) {
-                val permission = arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                )
-                requestPermissions(
-                    permission, REQUEST_CODE_LOCATION_PERMISSION
-                )
-            } else {
                 fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
 
                     val location: Location? = task.result
@@ -133,7 +155,6 @@ class MainActivity : ComponentActivity() {
             viewModel.getPrayerTimes(long!!, lat!!, Calendar.getInstance().formatDate())
         }
     }
-
 
 }
 
